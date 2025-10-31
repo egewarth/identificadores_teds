86964a218d4520b7b3428bb9014c517a22093644
João Egewarth <egewarth@gmail.com>
2025-10-31 14:50:06 -0300

    Adicionado mecanismo para remoção de notas de empenho que não representam TEDs, nos padrões: TED S/V. TED: S/VINCULO. METEDOLOGIAS (TED S/V) TED: S/N. ( TED/ SV.). TED: S/VINCULO. TED: S/N. TED S/V.

--- empenhos_teds.sql
+++ empenhos_teds.sql
@@ -1,63 +1,96 @@
 with
-empenhos_orgaos_1 as (
+empenhos_sem_vinculo_ted as(
+  select
+    *,
+    right(ne_ccor, 12) as ne,
+    left(ne_ccor,6) as orgao_id,
+    null as nc,
+    null as num_transf,
+    'sem vinculo' as metodo
+  from siafi.empenhos_tesouro
+  where
+    ne_ccor_descricao ~* '\bTED[[:space:]:/().-]*(S/?[VN]|S/?VINCULO)'
+    or ne_ccor_descricao ~* 'SEM[[:space:]]+VINC[[:space:]]*(ULO|/TED)'
+
+),
+empenhos_filtrados as(
+  select
+    *
+  from siafi.empenhos_tesouro
+  where 
+    ne_ccor_descricao !~* '\bTED[[:space:]:/().-]*(S/?[VN]|S/?VINCULO)'
+    and ne_ccor_descricao !~* 'SEM[[:space:]]+VINC[[:space:]]*(ULO|/TED)'
+
+),
+empenhos_orgaos_metodo_1 as (
   select
       *,
       -- Uma série de extrações que servirão de identificadores 
       right(ne_ccor, 12) as ne,
+      left(ne_ccor,6) as orgao_id,
+      regexp_substr(ne_ccor_descricao, '([0-9]{4}NC[0-9]+)') as nc,
       replace(
-          (
-              regexp_match(
-                  ne_ccor_descricao,
-                  '(FERENCIA|NUMERO|Nº|TED|CRICAO|TRANSF.|CAO|TRANSFERENCIA )(\s|^|-|)([0-9]{6}|1\w{5}|[0-9]{3}\.[0-9]{3})(\s|$|\.|,|-|\/)'
-              )
-          )[3],
+        (regexp_match(
+          ne_ccor_descricao,
+          '(?:TED|(?:NUM[[:space:].-]*)?(?:TRANSFERENCIA|TRANSF(?:\.|\:)|TRANF(?:\.|\:))|N[ºo]|NUMERO|CRICAO)[[:space:].:-]*([0-9]{6}|1[A-Z0-9]{5}|[0-9]{3}\.[0-9]{3})',
+          'i'
+        ))[1],
           '.',
           ''
       ) as num_transf,
-      regexp_substr(ne_ccor_descricao, '([0-9]{4}NC[0-9]+)') as nc
-  from siafi.empenhos_tesouro
-  where left(ne_ccor,6)= '113601'
+      'metodo 1' as metodo
+  from empenhos_filtrados
 ),
 
-empenhos_restantes as(
-select * from empenhos_orgaos_1 where num_transf is null AND nc is null
+empenhos_restantes_metodo_1 as(
+select * from empenhos_orgaos_metodo_1 where num_transf is null AND nc is null
 ),
 
-empenhos_orgaos_2 as (
+empenhos_orgaos_metodo_2 as (
   select
     -- seleciona todas as colunas do órgãos 1, exceto nc e num_transf
-      emissao_mes,emissao_dia,ne_ccor,ne_num_processo,ne_info_complementar,ne_ccor_descricao,doc_observacao,natureza_despesa,natureza_despesa_descricao,ne_ccor_favorecido,ne_ccor_favorecido_descricao,ne_ccor_ano_emissao,ptres,fonte_recursos_detalhada,fonte_recursos_detalhada_descricao,despesas_empenhadas,despesas_liquidadas,despesas_pagas,restos_a_pagar_inscritos,restos_a_pagar_pagos,dt_ingest,ne,
+      emissao_mes,emissao_dia,ne_ccor,ne_num_processo,ne_info_complementar,ne_ccor_descricao,doc_observacao,natureza_despesa,natureza_despesa_descricao,ne_ccor_favorecido,ne_ccor_favorecido_descricao,ne_ccor_ano_emissao,ptres,fonte_recursos_detalhada,fonte_recursos_detalhada_descricao,despesas_empenhadas,despesas_liquidadas,despesas_pagas,restos_a_pagar_inscritos,restos_a_pagar_pagos,dt_ingest,ne,orgao_id,nc,
       replace(
           (regexp_match(
             ne_ccor_descricao,
-            '.*(?:' ||
-              -- TED e variações
-              '(?:TED|TED[[:space:].-]*)' ||
-              '|' ||
-              -- SIAFI N∫
-              '(?:SIAFI[[:space:]]+N∫)' ||
-              '|' ||
-              -- N∞
-              '(?:N∞)' ||
-            ')[[:space:].-]*' ||   -- possíveis espaços, pontos ou hífens entre prefixo e número
-            '([A-Za-z0-9/]+)',     -- captura o código (alfanumérico e /)
+            '.*(?:NT|NOTA[[:space:]]+DE[[:space:]]+TRANSFERENCIA)[:.[:space:]]*((?=[A-Za-z0-9]*[0-9])[A-Za-z0-9]{6})',
             'i'
           ))[1],
           '.',
           ''
       ) as num_transf,
-      regexp_substr(ne_ccor_descricao, '([0-9]{4}NC[0-9]+)') as nc
-  from empenhos_restantes p
+      'metodo 2' as metodo
+  from empenhos_restantes_metodo_1 p
 ),
 
-empenhos_teds as (
-  select * from empenhos_orgaos_1 where num_transf is not null OR nc is not null
-  UNION ALL
-  select * from empenhos_orgaos_2 where num_transf is not null OR nc is not null
+empenhos_restantes_metodo_2 as(
+select
+emissao_mes,emissao_dia,ne_ccor,ne_num_processo,ne_info_complementar,ne_ccor_descricao,doc_observacao,natureza_despesa,natureza_despesa_descricao,ne_ccor_favorecido,ne_ccor_favorecido_descricao,ne_ccor_ano_emissao,ptres,fonte_recursos_detalhada,fonte_recursos_detalhada_descricao,despesas_empenhadas,despesas_liquidadas,despesas_pagas,restos_a_pagar_inscritos,restos_a_pagar_pagos,dt_ingest,ne,orgao_id,nc,num_transf,
+'vinculo nao encontrado' as metodo
+from empenhos_orgaos_metodo_2 where num_transf is null AND nc is null
+),
+
+empenhos_teds as(
+select * from empenhos_sem_vinculo_ted
+UNION ALL
+select * from empenhos_orgaos_metodo_1 where num_transf is not null OR nc is not null
+UNION ALL
+select * from empenhos_orgaos_metodo_2 where num_transf is not null OR nc is not null
+UNION ALL
+select * from empenhos_restantes_metodo_2
 )
 
-select * from empenhos_teds
-
-
-
-
+select
+  *
+from empenhos_teds
+where
+metodo = 'vinculo nao encontrado'
+AND
+((doc_observacao like '%TED%'
+AND
+doc_observacao not like '%METEDOLOGIAS%')
+OR
+(ne_ccor_descricao like '%TED%'
+AND
+ne_ccor_descricao not like '%METEDOLOGIAS%')
+)
